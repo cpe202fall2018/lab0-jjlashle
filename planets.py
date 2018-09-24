f6c4f1970a2aca79bdbe28c862ca7f6c4b0803cc
jjlashle <jjlashle@calpoly.edu>
2018-09-24 16:49:14 -0700

    added comments

--- planets.py
+++ planets.py
@@ -1,3 +1,14 @@
+#
+# Name: Josiah Lashley
+# ID: 014136534
+# Date (9-24-2018)
+#
+# Lab 0
+# Section 13/14
+# Purpose of Lab (Environment, Python, and Testing!)
+# Additional Comments
+
+# Take inputed integer form user and transforms it into a float in context
 def weight_on_planets():
    e_w = int(input("What do you weigh on earth? "))
    m_w = e_w * 0.38
